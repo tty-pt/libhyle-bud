c27c50ad3d2378b3c25a352a177ba11a3ede90d7
Paulo Quirino <quirinpa@gmail.com>
2026-09-22 11:51:51 +0100

    second commit

--- objects-set.mk
+++
@@ -1,5 +0,0 @@
-CFLAGS-libhyle-bud-o := -fPIC -I../hyle/include -I../hyle-source/include -I../bud/include -I../libqmap/include
-CFLAGS-filter-o := -fPIC -I../hyle/include -I../hyle-source/include -I../bud/include -I../libqmap/include
-CFLAGS-table-o := -fPIC -I../hyle/include -I../hyle-source/include -I../bud/include -I../libqmap/include
-CFLAGS-picker-o := -fPIC -I../hyle/include -I../hyle-source/include -I../bud/include -I../libqmap/include
-CFLAGS-form-o := -fPIC -I../hyle/include -I../hyle-source/include -I../bud/include -I../libqmap/include
